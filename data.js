3616604de121990fbd41ca0f92067c19a304cea3
Timothy Gaither <timjgaither@gmail.com>
2022-02-04 17:08:20 +0100

    push

---
+++ data.js
@@ -0,0 +1,162 @@
+icons=[
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+
+const containerIcons= document.getElementById('container-icons');
+const selector = document.getElementById('type-select');
+
+drawIcons(containerIcons, icons);
+
+
+//Filtro gli elementi in base all'opzione
+selector.addEventListener('change', function() {
+
+    let selection = this.value;
+
+    if (selection == '') {
+        drawIcons(containerIcons, icons);
+    } else {
+
+        let filteredIcons = icons.filter( icon => {
+            if (icon.type == selection) {
+                return true;
+            }
+            return false;
+        });
+
+        drawIcons(containerIcons, filteredIcons);
+    }
+});
+
+
+//Funzione per creare icone all'interno del container
+function drawIcons(container, icons) {
+
+    let content = '';
+
+    icons.forEach(oggetto => {
+
+        content += `<div class="icon">
+						<i style="color: ${oggetto.color};" class="${oggetto.family} ${oggetto.prefix}${oggetto.name}"></i>
+						<div class="icon-text">${oggetto.name}</div>
+                    </div>`;
+    });
+
+    container.innerHTML = content;
+
+}
+
+
+
+
