bc7f4a9e8c1d6812194acfee3fde7ebad53857bd
Timothy Gaither <timjgaither@gmail.com>
2022-02-06 18:16:51 +0100

    push esercizio

--- data.js
+++ data.js
@@ -114,10 +114,33 @@ icons=[
 ];
 
 const containerIcons= document.getElementById('container-icons');
-const selector = document.getElementById('type-select');
+let selector = document.getElementById('type-select');
+let btnColor=document.getElementById('btn');
 
 drawIcons(containerIcons, icons);
 
+//Cambio di colore di tutte le icone in base al click
+btnColor.addEventListener('click',
+	function(){
+		
+		
+		let content = '';
+		
+		icons.forEach(oggetto => {
+			itemColor='#'+ hexColour(getRandomNumber(0, 255),getRandomNumber(0, 255),getRandomNumber(0, 255));
+
+
+			content += `<div class="icon">
+							<i style="color: ${itemColor};" class="${oggetto.family} ${oggetto.prefix}${oggetto.name}"></i>
+							<div class="icon-text">${oggetto.name}</div>
+						</div>`;
+		});
+					
+					console.log(content)
+		
+					containerIcons.innerHTML=content
+	}
+)
 
 //Filtro gli elementi in base all'opzione
 selector.addEventListener('change', function() {
@@ -139,6 +162,14 @@ selector.addEventListener('change', function() {
     }
 });
 
+//creo i valori della select
+let valueSelect=selector.value;
+
+valueSelect+=`  <option value="">All</option>
+				<option value="animal">Animal</option>
+				<option value="vegetable">Vegetable</option>
+				<option value="user">User</option>`;
+selector.innerHTML+= valueSelect
 
 //Funzione per creare icone all'interno del container
 function drawIcons(container, icons) {
@@ -157,6 +188,24 @@ function drawIcons(container, icons) {
 
 }
 
+function hexColour(c,a,b) {
+	let valore;
+	let valore2;
+	let valore3;
+	let risultato;
+	if (c < 256 && a<256 && b<256) {
+	  valore= Math.abs(c).toString(16);
+	  valore2= Math.abs(a).toString(16);
+	  valore3= Math.abs(b).toString(16);
+	  risultato=valore+valore2+valore3
+	  return risultato;
+	}
+	
+}
+  
+ 
 
-
-
+  function getRandomNumber(min, max){
+    return Math.floor(Math.random() * (max - min +1)) + min;
+}
+  
